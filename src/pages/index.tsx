55674cbddc4339beacf5347060301e3aeef2db2c
Dawid Rodak <dawidrodak@yahoo.com>
2022-08-12 12:01:51 +0200

    Scroll to bottom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Button, Input, Spinner } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { useEffect, useRef } from 'react'
 import { useQueryClient } from 'react-query'
 import { useInputState } from '../components/hooks'
 import { trpc } from '../utils/trpc'
 
 const Home: NextPage = () => {
     const [text, setText] = useInputState('')
+    const listRef = useRef<HTMLUListElement>(null)
 
     const qc = useQueryClient()
     const addWords = trpc.useMutation(['words.add'], {
@@ -16,6 +18,13 @@ const Home: NextPage = () => {
     })
     const words = trpc.useQuery(['words.getAll'])
 
+    useEffect(() => {
+        const list = listRef.current
+        if (list) {
+            list.scrollTop = list.scrollHeight
+        }
+    }, [words.data])
+
     const renderContent = () => {
         if (words.isLoading) {
             return <Spinner />
@@ -23,7 +32,10 @@ const Home: NextPage = () => {
 
         if (words.data?.length) {
             return (
-                <ul className="h-full overflow-y-auto flex flex-col gap-y-2 w-full items-center pb-3">
+                <ul
+                    ref={listRef}
+                    className="h-full overflow-y-auto flex flex-col gap-y-2 w-full items-center pb-3"
+                >
                     {words.data.map((word) => (
                         <li key={word.id}>{word.text}</li>
                     ))}
@@ -34,8 +46,9 @@ const Home: NextPage = () => {
 
     return (
         <main className="flex flex-col gap-y-4 items-center h-[100vh] w-[100vw]">
+            {renderContent()}
             <form
-                className="flex gap-x-5 w-[400px] mt-5"
+                className="flex gap-x-5 w-[400px] my-10"
                 onSubmit={(e) => {
                     e.preventDefault()
                     addWords.mutate({ text })
@@ -55,7 +68,6 @@ const Home: NextPage = () => {
                     Send
                 </Button>
             </form>
-            {renderContent()}
         </main>
     )
 }
