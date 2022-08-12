496d8767c05b5c5a0cc95027da585f21fa37c573
Dawid Rodak <dawidrodak@yahoo.com>
2022-08-12 08:53:44 +0200

    list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,21 +17,25 @@ const Home: NextPage = () => {
     const words = trpc.useQuery(['words.getAll'])
 
     const renderContent = () => {
-        if (words.isFetching) {
+        if (words.isLoading) {
             return <Spinner />
         }
 
         if (words.data?.length) {
-            return words.data.map((word) => (
-                <span key={word.id}>{word.text}</span>
-            ))
+            return (
+                <ul className="h-full overflow-y-auto flex flex-col gap-y-2 w-full items-center pb-3">
+                    {words.data.map((word) => (
+                        <li key={word.id}>{word.text}</li>
+                    ))}
+                </ul>
+            )
         }
     }
 
     return (
-        <main className="flex flex-col gap-y-4 items-center justify-center h-[100vh] w-[100vw]">
+        <main className="flex flex-col gap-y-4 items-center h-[100vh] w-[100vw]">
             <form
-                className="flex gap-x-5 w-[400px]"
+                className="flex gap-x-5 w-[400px] mt-5"
                 onSubmit={(e) => {
                     e.preventDefault()
                     addWords.mutate({ text })
